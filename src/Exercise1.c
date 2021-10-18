89f952727e981e0113909454dfe2be8d34eff317
HoangphucNguyen1110 <80943005+HoangphucNguyen1110@users.noreply.github.com>
2021-10-18 17:08:22 +0700

    up ex 1 (3)

--- src/Exercise1.c
+++ src/Exercise1.c
@@ -16,8 +16,8 @@ void Ex1(int arr[], int n){
 	//Your codes here
     int i,j;
 	int sym=1;
-	for (i=0,j=n-1;i<j;i++,j--) {
-		if (arr[i]!= arr[j]) {
+	for (i=0,j=n-1;(i<=floor((n-1)/2) && (j>= ceil (( n-1)/2)));i++,j--){
+		if (arr[j]!= arr[i]) {
 			sym=0;
 		}
 	}
